ec658ce9fe47f5272a406de146ff74fb1d813bf1
MRCG-djeffries <djeffries@mrc.gm>
2026-01-22 15:57:53 +0000

    Comment out manifest.json second in service worker
    Comment out the inclusion of manifest.json in the service worker.

--- shinylive-sw.js
+++ shinylive-sw.js
@@ -2236,7 +2236,7 @@ self.addEventListener("install", (event) => {
         `${base_path}/app.json`,
         `${base_path}/manifest.json`,
         `${base_path}/shinylive/webr/vfs/usr/lib/R/library/translations/DESCRIPTION`,
-        `${base_path}/manifest.json`,
+        //`${base_path}/manifest.json`,
         `${base_path}/icon-192.png`,
         `${base_path}/icon-512.png`,
       ]);
