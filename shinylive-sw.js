7225ddc29bffc4e09b962bdc1e4f671ff5bee8f4
MRCG-djeffries <djeffries@mrc.gm>
2026-01-27 17:10:43 +0000

    Update service worker version to v12

--- shinylive-sw.js
+++ shinylive-sw.js
@@ -2205,7 +2205,7 @@ function asgiToRes(res, body) {
 // src/shinylive-sw.ts
 var useCaching = false;
 var cacheName = "::shinyliveServiceworker";
-var version = "v11";
+var version = "v12";
 function addCoiHeaders(resp) {
   const headers = new Headers(resp.headers);
   headers.set("Cross-Origin-Embedder-Policy", "credentialless");
