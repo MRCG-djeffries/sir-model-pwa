f659beaa8026c8418f457f2ca5ec8acdde07323f
MRCG-djeffries <djeffries@mrc.gm>
2026-01-28 16:28:40 +0000

    Update shinylive-sw.js to v8

--- shinylive-sw.js
+++ shinylive-sw.js
@@ -2205,7 +2205,7 @@ function asgiToRes(res, body) {
 // src/shinylive-sw.ts
 var useCaching = false;
 var cacheName = "::shinyliveServiceworker";
-var version = "v7";
+var version = "v8";
 
 function addCoiHeaders(resp) {
   const headers = new Headers(resp.headers);
